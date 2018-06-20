152a7dfc4a30c911e285fab04920b3391d0596ba
Thibaut Lepage <thib.lepage@gmail.com>
2018-06-20 18:07:35 +0200

    feat(public_api): export public services

--- public_api.ts
+++ public_api.ts
@@ -1,1 +1,5 @@
 export * from './src/app/oauth2/oauth2.module';
+export * from './src/app/oauth2/services/guards/oauth2-connected.guard';
+export * from './src/app/oauth2/services/guards/oauth2-visitor.guard';
+export * from './src/app/oauth2/services/oauth2.service';
+
