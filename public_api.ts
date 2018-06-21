9e0f7720684b425d591c1bc80ff2eddd214dd54b
Thibaut Lepage <thib.lepage@gmail.com>
2018-06-21 14:23:36 +0200

    feat(public_api): export models

--- public_api.ts
+++ public_api.ts
@@ -1,5 +1,7 @@
+export * from './src/app/oauth2/models/oauth2-client-config.model';
+export * from './src/app/oauth2/models/openId-metadata';
+export * from './src/app/oauth2/models/openid-user';
 export * from './src/app/oauth2/oauth2.module';
 export * from './src/app/oauth2/services/guards/oauth2-connected.guard';
 export * from './src/app/oauth2/services/guards/oauth2-visitor.guard';
 export * from './src/app/oauth2/services/oauth2.service';
-
