d25dec83055a891b9f734555195450e234b95840
Thibaut Lepage <thib.lepage@gmail.com>
2018-07-02 14:37:02 +0200

    style(hero): removing useless code

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -7,26 +7,20 @@ import { Hero } from '../models/hero';
 
 @Injectable()
 export class HeroService {
-  private heroesUrl = 'api/heroes'; // URL to web api
+  private heroesUrl = 'api/heroes';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getHeroes() {
     return this.http
       .get<Hero[]>(this.heroesUrl)
-      .pipe(map(data => data), catchError(this.handleError));
+      .pipe(catchError(this.handleError));
   }
 
   getHero(id: number): Observable<Hero> {
-    return this.getHeroes().pipe(
-      map(heroes => {
-        const founded = heroes.find(hero => hero.id === id);
-        if (!founded) {
-          throw new Error(`No Hero found for id ${id}`);
-        }
-        return founded;
-      })
-    );
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.get<Hero>(url);
   }
 
   save(hero: Hero) {
@@ -37,33 +31,23 @@ export class HeroService {
   }
 
   delete(hero: Hero) {
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-
     const url = `${this.heroesUrl}/${hero.id}`;
 
     return this.http.delete<Hero>(url).pipe(catchError(this.handleError));
   }
 
-  // Add new Hero
   private post(hero: Hero) {
-    const headers = new Headers({
-      'Content-Type': 'application/json'
-    });
-
     return this.http
       .post<Hero>(this.heroesUrl, hero)
       .pipe(catchError(this.handleError));
   }
 
-  // Update existing Hero
   private put(hero: Hero) {
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-
     const url = `${this.heroesUrl}/${hero.id}`;
 
-    return this.http.put<Hero>(url, hero).pipe(catchError(this.handleError));
+    return this.http
+      .put<Hero>(url, hero)
+      .pipe(catchError(this.handleError));
   }
 
   private handleError(res: HttpErrorResponse | any) {
